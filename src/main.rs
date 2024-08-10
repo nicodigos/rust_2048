884617cbf5fd457568c1706380362df862c68dc5
Nicolas Peralta <nperaltapz@gmail.com>
2024-08-10 14:28:09 -0400

    20 iterations over the first implementation. Up direction doesn't work well, the rest of them seems to work properly

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
-use std::{collections::btree_map::Range, os::unix::fs::DirBuilderExt, vec};
-use std::ops::Range as RealRange;
+use std::vec;
+use std::ops::Range;
+use rand::Rng;
 
 fn main() {
 
-    let new_board = Board::default();
-    new_board.display_array()
+    let mut new_board = Board::default();
+    new_board.display_array();
+    let directions = vec![Direction::Up,
+                          Direction::Down,
+                          Direction::Left,
+                          Direction::Right,
+                        ];
+    let mut rng = rand::thread_rng();
+    for _i in 0..20 {
+        let random_index = rng.gen_range(0..directions.len());
+        match &directions[random_index] {
+            Direction::Up => println!("up"),
+            Direction::Down => println!("down"),
+            Direction::Left => println!("left"),
+            Direction::Right => println!("right"),
+        }
 
+        new_board.movement(directions[random_index]);
+        new_board.display_array();
+    }
+    
+   
 }
 
 struct Board {
@@ -24,6 +44,8 @@ impl Default for Board {
     }
     
 }
+
+#[derive(Copy, Clone, Debug)]
 enum Direction {
     Up,
     Down,
@@ -32,7 +54,7 @@ enum Direction {
 }
 
 impl Board {
-    fn display_array(self) {
+    fn display_array(&self) {
         for array in self.array.iter() {
             let mut row_str:String = String::from("[ ");
             for element in array.iter() {
@@ -40,25 +62,59 @@ impl Board {
             }
             row_str.push_str("]");
             println!("{}", row_str);
-        }        
+        }
+        println!()        
     }
 
-    fn movement(&mut self, direction: Direction) {
+    fn movement(&mut self, direction:Direction) {
+        let mut rearranged_board = Board::static_rearrange_board(&mut self.array.clone(),&direction);
+        for array in &mut rearranged_board {
+            let num_of_zeroes = array.iter().filter(|&&x| x == 0).count();
+            array.retain(|&x| x != 0);
+            for i in (1..array.len()).rev() {
+                if array[i] == array[i-1] {
+                    array[i] = array[i] * 2;
+                    array[i-1] = 0;
+                    array.retain(|&x| x != 0);
+                    array.insert(0,0);
+                }
+            }
+
+            for i in (0..num_of_zeroes) {
+                array.insert(0,0);
+            }
+        }
+
+        let mut zeroes_positions:Vec<Vec<usize>> = vec![];
+        for r in 0..4 {
+            for c in 0..4 {
+                if rearranged_board[r][c] == 0 {
+                    *&mut zeroes_positions.push(vec![r,c]);
+                }
+            }
+        }
+
+        // When a movement is done a random 2 appears
+        if zeroes_positions.len() > 0 {
+            let mut rng = rand::thread_rng();
+            let random_index = rng.gen_range(0..zeroes_positions.len());
+            let selected_position = &zeroes_positions[random_index];
+            *&mut rearranged_board[selected_position[0]][selected_position[1]] = 2;
+        }
+
+        *&mut self.array = Board::static_rearrange_board(&mut rearranged_board, &direction);
+
 
     }
 
-    fn check_available_movement(&self, direction:Direction) {
-
-    }
-
-    fn rearrange_board(&self, direction: Direction) -> Vec<Vec<i16>> {
-        let mut copy_array = self.array.clone();
-
-        match direction {
-            Direction::Up => Board::vertical_rearrange((0..4),
+    fn static_rearrange_board(array: &mut Vec<Vec<i16>>, direction: &Direction) -> Vec<Vec<i16>> {
+        let mut copy_array = array.clone();
+        
+        match *direction {
+            Direction::Up => Board::static_vertical_rearrange(0..4,
                                     "rev",
                                                     copy_array),
-            Direction::Down => Board::vertical_rearrange((0..4),
+            Direction::Down => Board::static_vertical_rearrange(0..4,
                                 "none",
                                     copy_array),
             Direction::Left => {for array in &mut copy_array {
@@ -67,12 +123,13 @@ impl Board {
                                 copy_array
                                 },
             Direction::Right => copy_array
+
         }
 
         
     }
 
-    pub fn vertical_rearrange(range_rows: RealRange<usize>, 
+    pub fn static_vertical_rearrange(range_rows: Range<usize>, 
                           range_instructions: &str,
                           array_2d: Vec<Vec<i16>>) -> Vec<Vec<i16>> {
 
@@ -90,7 +147,7 @@ impl Board {
                 for c in 0..4 {
                     // iterate over rows of the copy_array
                     for r in &vec_range {
-                        array.push(array_2d[c][*r].clone())
+                        array.push(array_2d[*r][c].clone())
                     }
                     new_array.push(array.clone());
                     array.clear();    
